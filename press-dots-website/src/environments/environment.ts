5ebef8b0b7e8fa78e22b1637c8e63d51ac8090d2
Majid Ali <majid.ali@synergy-it.com>
2023-01-31 16:27:45 +0500

    Compile version

--- press-dots-website/src/environments/environment.ts
+++ press-dots-website/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl:'https://dev-pressdots-api.azurewebsites.net/api/v1/'
+  apiUrl:'https://dev-pressdot-api.azurewebsites.net/api/v1/'
 };
 
 /*
